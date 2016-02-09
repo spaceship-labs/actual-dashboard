30e7ab0c01974150237bab967fe1cb3e8f1a1719
Luis Perez <luis19prz@gmail.com>
2016-02-09 15:25:47 -0500

    test commit

--- src/app/index.api.js
+++ src/app/index.api.js
@@ -174,8 +174,8 @@
 
         // Base Url
         //api.baseUrl = 'app/data/';
-        //api.baseUrl = 'http://actual-api.herokuapp.com';
-        api.baseUrl = 'http://localhost:1337';
+        api.baseUrl = 'http://actual-api.herokuapp.com';
+        //api.baseUrl = 'http://localhost:1337';
 
         api.sample = $resource(api.baseUrl + 'sample/sample.json');
 
