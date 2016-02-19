54bc69f23e24ce52cd864be798a659c619ba483f
Luis <luis19prz@gmail.com>
2016-02-18 19:43:10 -0500

    product lists

--- src/app/main/products/list/list.controller.js
+++ src/app/main/products/list/list.controller.js
@@ -13,31 +13,6 @@
 
         // Data
         vm.products = Products.data;
-        console.log(vm.products);
-
-        console.log(api.product.find.get().$promise);
-
-        /*
-        vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-          console.log(api.product.find.get().$promise);
-          return api.product.find.get().$promise;
-        }).withPaginationType('full_numbers');
-        */
-        /*vm.dtOptions = DTOptionsBuilder.fromFnPromise(function(d) {
-          var defer = $q.defer();
-          api.product.find.get({'page':2}).$promise.then(function(res){
-            console.log(res);
-            defer.resolve(res.data);
-          })
-          return defer.promise;
-        })
-        .withDataProp('data')
-        .withPaginationType('simple')
-        .withOption('responsive', true)
-        .withOption('autoWidth',false)
-        .withOption('processing', true)
-        .withOption('dom','<"top"f>rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>')
-        */
 
         vm.dtOptions = DTOptionsBuilder
                     .newOptions()
@@ -46,22 +21,21 @@
                     .withOption('processing', true)
                     .withOption('serverSide', true)
                     .withOption('paging', true)
+                    .withOption('responsive',true)
+                    .withOption('autoWidth',true)
                     .withPaginationType('numbers')
-                    .withDOM('<"top">t<i"bottom"p><"clear">');
+                    .withDOM('<"top"f>rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>');
 
         function serverData(sSource, aoData, fnCallback, oSettings) {
-                    console.log(aoData);
-                    console.log(sSource);
-                    console.log(oSettings);
-
                     //All the parameters you need is in the aoData variable
                     var draw = aoData[0].value;
                     var order = aoData[2].value;
                     var start = aoData[3].value;
                     var length = aoData[4].value;
 
+                    console.log(aoData);
 
-                    api.product.find.get({'page':start+1},
+                    api.product.find.get({'page':start},
                       function(res){
                         var records = {
                             'draw': draw,
@@ -74,24 +48,7 @@
                       function(err){
                         console.log(err);
                       }
-                    )
-
-
-
-
-
-                    /*
-                    //Then just call your service to get the records from server side
-                    filterService.execute(start, length, order).then(function(result){
-
-                        var records = {
-                                'draw': draw,
-                                'recordsTotal': result.total,
-                                'recordsFiltered': result.filtered,
-                                'data': result.records
-                            };
-                        fnCallback(records);
-                    });*/
+                    );
                 }
 
         vm.dtColumns = [
