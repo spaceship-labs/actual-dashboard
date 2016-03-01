593c4e3548bc8ab86b80d011e44a1719dee935a3
Luis <luis19prz@gmail.com>
2016-02-29 19:37:57 -0500

    search params url

--- src/app/main/products/search/search.controller.js
+++ src/app/main/products/search/search.controller.js
@@ -33,12 +33,39 @@
         // Methods
 
         function init(){
-          vm.doSearch();
+          vm.doSearch(1,true);
           vm.lines.unshift({Code:false,Name:'Todos'});
+          vm.colors.unshift({Code:false,Name:'Todos'});
           vm.search.line = vm.lines[0].code;
+
+          //Reading params from url
+          var params = $location.search();
+          var aux = [];
+          var aux2 = [];
+          if(params.term){
+            vm.search.term = params.term;
+          }
+          if(params.color){
+            aux = vm.colors.filter(function(col){
+              return col.Code == params.color;
+            });
+            if(aux.length > 0){
+              vm.search.color = aux[0].Code;
+            }
+          }
+
+          if(params.line){
+            aux2 = vm.lines.filter(function(line){
+              return line.Code == params.line;
+            });
+            if(aux2.length > 0){
+              vm.search.line = aux2[0].Code;
+            }
+          }
+
         }
 
-        function doSearch(_page){
+        function doSearch(_page, firstSearch){
           var length = vm.length;
           var query = {};
           var page = _page || 1;
@@ -56,8 +83,22 @@
               query.page = page;
           }
 
-          query.line = vm.search.line;
-          query.color = vm.search.color;
+          query.line = vm.search.line || null;
+          query.color = vm.search.color || null;
+
+          if(query.line == 'false') query.line = null;
+          if(query.color == 'false') query.color = null;
+
+          if(!firstSearch){
+              if(query.page){
+                $location.search('page',query.page);
+              }
+
+              $location.search('term',vm.search.term);
+              $location.search('line', query.line);
+              $location.search('color', query.color);
+          }
+
 
           vm.apiResource(query,
             function(res){
