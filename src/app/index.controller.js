4ca93037813838d653d91bbbfe99118b15051a70
Luis <luis19prz@gmail.com>
2017-01-06 12:28:17 -0500

    added more options to tinymce editor

--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -57,7 +57,13 @@
 
        $scope.tinymceOptions = {
           //plugins: 'link wordcount',
-          plugins: 'charactercount',
+          plugins: [
+            'charactercount advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+          ],
+          toolbar1: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
           menu:{
             edit   : {title : 'Edit'  , items : 'undo redo | cut copy paste pastetext | selectall'},
             insert : {title : 'Insert', items : 'link media | template hr'},
