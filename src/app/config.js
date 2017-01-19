cc3b4ba21f9b89ebb288de31eb871c8a265e4227
Luis <luis19prz@gmail.com>
2017-01-19 15:08:12 -0500

    token prefix by env2

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 angular.module("envconfig", [])
-.constant("dev", {"baseUrl":"http://localhost:1337"})
-.constant("sandbox", {"baseUrl":"http://sandbox-actual-api.herokuapp.com"})
-.constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com"})
-.constant("production", {"baseUrl":"http://166.78.47.146"})
-.constant("ENV", {"baseUrl":"http://localhost:1337","name":"dev"});
+.constant("dev", {"baseUrl":"http://localhost:1337","tokenPrefix":"dev"})
+.constant("sandbox", {"baseUrl":"http://sandbox-actual-api.herokuapp.com","tokenPrefix":"sandbox"})
+.constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com","tokenPrefix":"demo"})
+.constant("production", {"baseUrl":"http://166.78.47.146","tokenPrefix":"production"})
+.constant("ENV", {"baseUrl":"http://localhost:1337","tokenPrefix":"dev","name":"dev"});
