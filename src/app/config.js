d2d0076a5151277cd9296d7f519271c82b36c9d0
Luis <luis19prz@gmail.com>
2017-02-15 10:18:37 -0500

    envconfig default dev

--- src/app/config.js
+++ src/app/config.js
@@ -3,4 +3,4 @@ angular.module("envconfig", [])
 .constant("sandbox", {"baseUrl":"http://sandbox-actual-api.herokuapp.com","tokenPrefix":"sandbox"})
 .constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com","tokenPrefix":"demo"})
 .constant("production", {"baseUrl":"http://166.78.47.146","tokenPrefix":"production"})
-.constant("ENV", {"baseUrl":"http://localhost:1337","tokenPrefix":"dev","name":"dev"});
+.constant("ENV", {"baseUrl":"http://166.78.47.146","tokenPrefix":"production","name":"production"});
