a9c34ff52078c01eb9d42f0ed963e238ea246e50
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-09-06 12:44:00 -0500

    ewallet config module finished

--- src/app/main/configuration/ewallet/ewallet.controller.js
+++ src/app/main/configuration/ewallet/ewallet.controller.js
@@ -10,8 +10,10 @@
     $http,
     api,
     $scope,
-    zipcodeService,
-    dialogService
+    $timeout,
+    commonService,
+    dialogService,
+    ewalletService
   ) {
     var vm = this;
 
@@ -19,13 +21,78 @@
       myPickerEndDate: {},
       onSelectEndDate: onSelectEndDate,
       init: init,
+      update: update,
     });
 
     init();
 
     function init() {
-      console.log('Controller Init');
+      vm.isLoading = true;
+      ewalletService
+        .find()
+        .then(function(res) {
+          console.log('response: ', res.data);
+          vm.ewallet = res.data[0] || {};
+          vm.ewallet.exchangeRate = vm.ewallet.exchangeRate || 0;
+          vm.ewallet.maximumPercentageToGeneratePoints =
+            vm.ewallet.maximumPercentageToGeneratePoints || 0;
+          vm.endTime = new Date(angular.copy(vm.ewallet.expirationDate));
+          vm.isLoading = false;
+          $timeout(function() {
+            vm.myPickerEndDate.setMaxDate(new Date(vm.ewallet.expirationDate));
+          }, 1000);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     }
-    function onSelectEndDate() {}
+    function onSelectEndDate(pikaday) {
+      vm.ewallet.expirationDate = pikaday._d;
+      vm.myPickerEndDate.setMaxDate(vm.ewallet.expirationDate);
+    }
+    function update(form) {
+      if (form.$valid) {
+        vm.isLoading = true;
+        vm.ewallet.expirationDate = commonService.combineDateTime(
+          vm.ewallet.expirationDate,
+          vm.endTime,
+          59
+        );
+        if (vm.ewallet.id) {
+          ewalletService
+            .update(vm.ewallet.id, vm.ewallet)
+            .then(function(res) {
+              console.log(res.data);
+              dialogService.showDialog(
+                'Configuración del monedero actualizada'
+              );
+              vm.isLoading = false;
+            })
+            .catch(function(err) {
+              console.log(err);
+              dialogService.showDialog('Error, revisa los datos');
+              vm.isLoading = false;
+            });
+        } else if (!vm.ewallet.id) {
+          ewalletService
+            .create(vm.ewallet)
+            .then(function(res) {
+              console.log(res.data);
+              dialogService.showDialog(
+                'Configuración del monedero actualizada'
+              );
+              vm.isLoading = false;
+            })
+            .catch(function(err) {
+              console.log(err);
+              dialogService.showDialog('Error, revisa los datos');
+              vm.isLoading = false;
+            });
+        }
+      } else {
+        console.log(form);
+        dialogService.showDialog('Datos incompletos');
+      }
+    }
   }
 })();
--- src/app/main/services/ewallet.service.js
+++ src/app/main/services/ewallet.service.js
@@ -8,17 +8,23 @@
     var service = {
       update: update,
       find: find,
+      create: create,
     };
 
     return service;
+
+    function create(params) {
+      var url = '/ewalletconfiguration';
+      return api.$http.post(url, params);
+    }
 
     function update(id, params) {
       var url = '/ewalletconfiguration/' + id;
       return api.$http.post(url, params);
     }
 
-    function find(id) {
-      var url = '/ewalletconfiguration/' + id;
+    function find() {
+      var url = '/ewalletconfiguration/';
       return api.$http.get(url);
     }
   }
