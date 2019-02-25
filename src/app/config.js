316b180933d6ba2e0285a29a170b773196802012
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-02-25 15:24:29 -0500

    staged api added

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,24 @@
-angular.module("envconfig", [])
-.constant("dev", {"baseUrl":"http://localhost:1337","tokenPrefix":"dev"})
-.constant("sandbox", {"baseUrl":"https://sandboxapi.miactual.com","tokenPrefix":"sandbox"})
-.constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com","tokenPrefix":"demo"})
-.constant("production", {"baseUrl":"http://166.78.47.146","tokenPrefix":"production"})
-.constant("ENV", {"baseUrl":"http://localhost:1337","tokenPrefix":"dev","name":"dev"});
+angular
+  .module('envconfig', [])
+  .constant('dev', { baseUrl: 'http://localhost:1337', tokenPrefix: 'dev' })
+  .constant('sandbox', {
+    baseUrl: 'https://sandboxapi.miactual.com',
+    tokenPrefix: 'sandbox',
+  })
+  .constant('staged', {
+    baseUrl: 'https://stagedapi.miactual.com',
+    tokenPrefix: 'sandbox',
+  })
+  .constant('demo', {
+    baseUrl: 'http://demo-actual-api.herokuapp.com',
+    tokenPrefix: 'demo',
+  })
+  .constant('production', {
+    baseUrl: 'http://166.78.47.146',
+    tokenPrefix: 'production',
+  })
+  .constant('ENV', {
+    baseUrl: 'http://localhost:1337',
+    tokenPrefix: 'dev',
+    name: 'dev',
+  });
