23f695d4426d62a27478cf6c23e890559b418974
brandonNavarrete <brandon@spaceshiplabs.com>
2019-03-14 16:48:23 -0500

    show files report

--- src/app/main/users/users.module.js
+++ src/app/main/users/users.module.js
@@ -1,35 +1,32 @@
-(function ()
-{
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('app.users', [
-            'app.users.list',
-            'app.users.edit',
-            'app.users.create'
-        ])
-        .config(config);
+  angular
+    .module('app.users', [
+      'app.users.list',
+      'app.users.edit',
+      'app.users.create',
+    ])
+    .config(config);
 
-    /** @ngInject */
-    function config(msNavigationServiceProvider)
-    {
-        // Navigation
-        msNavigationServiceProvider.saveItem('users', {
-            title : 'Usarios',
-            group : false,
-            weight: 2,
-            state: 'app.users'
-        });
+  /** @ngInject */
+  function config(msNavigationServiceProvider) {
+    // Navigation
+    msNavigationServiceProvider.saveItem('users', {
+      title: 'Usuarios',
+      group: false,
+      weight: 2,
+      state: 'app.users',
+    });
 
-        msNavigationServiceProvider.saveItem('users.list', {
-            title: 'Lista de usuarios',
-            state: 'app.users_list'
-        });
+    msNavigationServiceProvider.saveItem('users.list', {
+      title: 'Lista de usuarios',
+      state: 'app.users_list',
+    });
 
-        msNavigationServiceProvider.saveItem('users.create', {
-            title: 'Registrar usuarios',
-            state: 'app.users_create'
-        });
-
-    }
+    msNavigationServiceProvider.saveItem('users.create', {
+      title: 'Registrar usuarios',
+      state: 'app.users_create',
+    });
+  }
 })();
