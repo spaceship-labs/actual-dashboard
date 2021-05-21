19a62dd497e07ea4ebabcfa50e5f4bc9fb62ae0c
Sergio Can <gersiopumas@gmail.com>
2021-05-21 12:57:05 -0500

    fix tinymce

--- src/app/config.js
+++ src/app/config.js
@@ -3,4 +3,4 @@ angular.module("envconfig", [])
 .constant("sandbox", {"baseUrl":"http://166.78.47.146:8080","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"sandbox"})
 .constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"demo"})
 .constant("production", {"baseUrl":"http://api.miactual.com","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"production"})
-.constant("ENV", {"baseUrl":"http://localhost:1337","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"dev","name":"dev"});
+.constant("ENV", {"baseUrl":"http://api.miactual.com","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"production","name":"production"});
